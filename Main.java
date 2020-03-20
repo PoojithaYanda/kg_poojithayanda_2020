be725588790be9ca86a43a3f5c86fe36c5a5d023
PoojithaYanda <62445864+PoojithaYanda@users.noreply.github.com>
2020-03-20 15:56:25 -0500

    case when strings have different length

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 /**
- * Description: This class Determine whether a one-to-one character mapping exists from
+ * Description: This class determines whether a one-to-one character mapping exists from
  * one string, s1, to another string, s2.
  * Author: Poojitha Yanda
  */
@@ -8,11 +8,23 @@ class Main
 {
     public static boolean mapCharacters(String firstString,String secondString)
     {
-        return true;
+        if(firstString.length()!=secondString.length())
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
     public static void main(String args[])
     {
+        if(args.length!=2)
+        {
+            System.out.println("Please provide two inputs. Example: java Main <first> <second>");
+            return;
+        }
         boolean result = mapCharacters(args[0],args[1]);
         System.out.println(result);
     }
