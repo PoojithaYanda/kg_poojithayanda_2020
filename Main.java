59e9ed273ab10b1774147b135112ff7977fc1933
PoojithaYanda <62445864+PoojithaYanda@users.noreply.github.com>
2020-03-20 15:47:21 -0500

    Initial commit with the Main Class structure

---
+++ Main.java
@@ -0,0 +1,19 @@
+/**
+ * Description: This class Determine whether a one-to-one character mapping exists from
+ * one string, s1, to another string, s2.
+ * Author: Poojitha Yanda
+ */
+
+class Main
+{
+    public static boolean mapCharacters(String firstString,String secondString)
+    {
+        return true;
+    }
+
+    public static void main(String args[])
+    {
+        boolean result = mapCharacters(args[0],args[1]);
+        System.out.println(result);
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,6 @@
+Steps to execute this code:
+
+Step-1: Compile the code - javac Main.java
+
+Step-2: Execute the code - java Main <firstString> <secondString>
+
